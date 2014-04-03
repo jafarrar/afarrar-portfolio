0a82a945081c84ec78af35ba62e9acba4ce7d2f9
Andrew Farrar <kegwen@gmail.com>
2014-04-03 14:00:26 -0400

    initial commit

---
+++ app/controllers/site_controller.rb
@@ -0,0 +1,7 @@
+class SiteController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Portfolio::Application.config.secret_key_base = '1bb54d67685880c6f9ec719056536ae0a47b33ba8eb6842d2d0a4c1d5e8fb48dfc54bd92ed7857746132f94b64d9385b188bbfb65af27067c8d087c2b578ff73'
